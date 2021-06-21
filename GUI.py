6ba71e15cf523d23f922e61621374466e8e0aa75
Partha <parthasarker3@gmail.com>
2021-06-22 00:38:21 +0600

    new cars and move image file added

--- GUI.py
+++ GUI.py
@@ -2,17 +2,22 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 import tkinter.font as font
 from PIL import ImageTk, Image
-import main
+import shutil
+import os
+import detector
 forget_list = []
+
+current_file = ''
 
 
 def get_car_from_files():
-    global forget_list
+    global forget_list, current_file
     for widget in forget_list:
         widget.pack_forget()
     forget_list = []
 
     car_location = askopenfilename()
+    current_file = car_location
     if car_location == '' or car_location is None:
         return
 
@@ -30,12 +35,17 @@ def get_car_from_files():
     space = Label(root)
     space.pack()
 
-    license_number = main.get_license_number(car_location)
+    license_number = detector.get_license_number(car_location)
     license_label = Label(root, text=f'License Plate Number: {license_number}')
     license_font = font.Font(size=15)
     license_label['font'] = license_font
     license_label.pack()
     forget_list.extend([panel, space, license_label])
+
+
+def move_file():
+    file_name = os.path.basename(current_file)
+    shutil.move(current_file, f'good/{file_name}')
 
 
 root = Tk()
@@ -48,4 +58,12 @@ file_chooser_button['font'] = button_font
 file_chooser_button.pack()
 space = Label(root)
 space.pack()
+
+space = Label(root)
+space.pack()
+file_mover_button = Button(root, text='Move Car', command=move_file)
+button_font = font.Font(size=15)
+file_mover_button['font'] = button_font
+file_mover_button.pack()
+
 root.mainloop()
